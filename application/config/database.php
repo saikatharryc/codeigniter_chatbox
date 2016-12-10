f060b4a64aa944870740d2ec5de8906abaefc1d3
Saikat Chakrabortty <saikatchakrabortty2@gmail.com>
2016-12-10 22:19:29 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => '',
 	'database' => 'chat',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'mongo_db',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
